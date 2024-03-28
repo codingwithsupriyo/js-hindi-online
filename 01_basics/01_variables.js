250e9995e8751dda1c0d539d8deb2b9b413e35d2
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-03-28 16:40:21 +0000

    variable knowledge

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2022020
+let accountEmail = "mongodb@gmail.com"
+var accountPass = "20202020"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 203030
+accountEmail = "sup@gmail.com"
+accountPass = "205050"
+accountCity = "kolkata"
+
+console.log(accountId);
+
+//multiple variable print in console table
+console.table([accountEmail,accountPass,accountCity,accountState]);
