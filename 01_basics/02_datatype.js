7f63bb68973d6d64089dc64147255dc894d23d19
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-03-30 12:29:13 +0000

    datatype all covered

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict"; //all js code for new version
+
+//datatype
+//alert (3+3) //we are using node js not browser
+//alert("supriyo")
+console.log("supriyo")
+console.log(3
+    +3); //this is not a Good write to code 
+    
+    //datatype
+    //number=>2 to power 53
+    //bigint=>use for big amount of number
+    //string=>""
+    //boolean=>true or false
+    //null=>standalone value //object
+    //undefined=>value not assigned
+    //symbol=>unique
+
+    let name = "supriyo"
+    let age = 21
+    let quality = true
+    let isLoggedIn = null
+    let married;
+
+   /* console.log(typeof(name)); //string
+    console.log(typeof(age)); //number
+    console.log(typeof(quality)); //boolean
+    console.log(typeof isLoggedIn); //object
+    console.log(typeof married); //undefined
+    */
+   console.table([typeof name, typeof age,typeof quality,typeof isLoggedIn,typeof married]);
