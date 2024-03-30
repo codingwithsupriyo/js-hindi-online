af15397902e0bd91135397bd0de0f67be454c8b5
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-03-30 13:32:00 +0000

    typeConversionandoperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+"use strict"; //used for all js code as newer version
+
+//let score = "33"
+
+//let valueInnumber = Number(score)
+//let valueInboolean = Boolean(score)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 ,false=>0
+
+//console.log(valueInboolean)
+
+//console.log(typeof valueInboolean)
+
+
+//conversion 2nd experiment
+/*let name = "supriyo"
+let isstringToboolean = Boolean(name);
+console.log(isstringToboolean);
+console.log(typeof isstringToboolean);*/
+
+//1=>true 2=>false
+//""=>false
+//"supriyo"=>true
+
+/*let Isnumber = 33
+let stringNumber = String(Isnumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+
+
+// *********** operation ************
+
+//basic arithmetic operation
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+//logical
+/*console.log(1+2+"2");
+console.log("2"+1+1);*/
+
+//this type of code using parenthisis
+//console.log((5+3) * 2 +4);
+
+//postfix or prefix basics
+let gameCounter = 100
+//gameCounter++;    output: 101
+//++gameCounter;   output: 101
+console.log(gameCounter);
+
+
+//Read prefix or postfix js mdn
+//read tc39 type-conversion
