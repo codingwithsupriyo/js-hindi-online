5aa12770a85f9d1dfcd4c6b1968dd5d9cf326f50
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-04-01 09:47:13 +0000

    summary datatype

---
+++ datatype_summary.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//primitive datatype : 7 types of premitive datatype = String,Number,Boolean,Null,undefined,BigInt,symbol
+/*
+let nameString = "supriyo";
+console.log(typeof nameString);
+
+let studentId = 11;
+console.log(typeof studentId);
+
+let attendStudent = true
+console.log(typeof attendStudent);
+
+let studentSubject = null
+console.log(typeof studentSubject);
+
+let opd;
+console.log(typeof opd);
+
+*/
+// Non primitive datatype : array , function , object   //function call function object
+/*let arr = ["supriyo","sarkar"];
+console.log(typeof arr);
+console.log(arr);
+*/
+/*let func = function(){
+   console.log("hello world") ;
+}
+console.log(typeof func);
+console.log(func);*/
+
+/*let obj = {
+    name : "supriyo",
+    age : 22
+}
+console.log(typeof obj);
+console.log(obj);*/
