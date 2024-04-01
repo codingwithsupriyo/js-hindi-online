01e0c274fdecffcb35c371bc27a911116e70b53d
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-04-01 09:13:10 +0000

    comparision method

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//always use same datatype comparison
+/*console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);*/
+
+//comparision convert null to a number treating 0 where 
+//console.log(null > 0); //false
+//console.log(null >=0); //true
+
+//using '===' for strictly check datatype and value with no conversion
+console.log("2"===2);
+console.log("2"==2);
