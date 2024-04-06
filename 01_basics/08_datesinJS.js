3f085eb9b85f84d3176e87e67b22882c259cecb1
codingwithsupriyo <165177240+codingwithsupriyo@users.noreply.github.com>
2024-04-06 03:31:49 +0000

    same

--- 01_basics/08_datesinJS.js
+++ 01_basics/08_datesinJS.js
@@ -1,11 +1,11 @@
 "use strict";
-let MyDate = new Date();
+//let MyDate = new Date();
 //console.log(typeof MyDate);
 //console.log(MyDate.toString());
 //console.log(MyDate.toDateString());
 //console.log(MyDate.toLocaleDateString());
 
-let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3);
 //console.log(myCreatedDate.toDateString());
 //console.log(myCreatedDate.toLocaleDateString());
 //console.log(myCreatedDate.toLocaleString());
@@ -14,9 +14,25 @@ let myCreatedDate = new Date(2023, 0, 23, 5, 3);
 console.log(myCreatedDate2.toLocaleString());*/
 
 //timestamp
-let myTimestamp = Date.now();
+//let myTimestamp = Date.now();
 //console.log(myCreatedDate.getTime());
 //console.log(myTimestamp);
 
 //Return milisecond to second
-console.log(Math.floor(myTimestamp/1000));
+//console.log(Math.floor(myTimestamp/1000));
+
+//method object customization
+
+/*let myObj = new Date();
+let result= myObj.toLocaleString('default',{
+    weekday:"long"
+});
+console.log(result);
+console.log(typeof result);*/
+
+let MyDate = new Date();
+//console.log(MyDate.getFullYear());
+//console.log(MyDate.getMilliseconds());
+let myTimestamp = Date.now();
+console.log(myTimestamp);
+console.log(MyDate.getTime());
